437441eab558667ff0df9d0bb3144154325eee64
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2017-12-06 16:33:00 +0100

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="hello">
+        <mia-header></mia-header>
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<script>
+
+    import Vue from 'vue';
+    import MiaHeader from './MiaHeader';
+
+    Vue.component('mia-header', MiaHeader);
+
+    export default {
+        name: 'HelloWorld',
+        components: {
+            MiaHeader,
+        },
+
+        data() {
+            return {
+                msg: 'Mia',
+            };
+        },
+    };
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+    h1, h2 {
+        font-weight: normal;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+
+</style>
